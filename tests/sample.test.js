01d9295f40f6fe008ea418cc2ace10d101cf10aa
sarmandjundi <sarmand.jundii@gmail.com>
2025-03-13 18:43:48 +0100

    added string matching test

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -2,3 +2,8 @@ test("Simple test example", () => {
     expect(2 + 2).toBe(4);
   });
   
+
+  test("check if string contains a word", ()=> {
+    const message = "hello, ci/cd pipeline!";
+    expect(message).toMatch(/CI\/CD/);
+  });
